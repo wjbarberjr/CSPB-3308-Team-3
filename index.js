09463ecd9c25abfda441fc9649542238573a76e2
Jordon Young <64931019+jordon-young@users.noreply.github.com>
2023-10-05 16:18:32 -0400

    Refactor cal_out() to create new rows in the output table Add clear_table() function to remove all table contents besides the header

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
-console.log("The script is running!");
+const table = document.getElementById('output');
+let total = 0;
 
-const body = document.querySelector('body');
+function cal_out() {
+    const calories = Number(document.getElementById("calories").value);
+    total += calories;
 
-console.log("The script finished!")
+    const new_row = document.createElement('tr');
+
+    const calories_consumed = document.createElement('td')
+    calories_consumed.innerText = total;
+    new_row.appendChild(calories_consumed);
+
+    const calories_entered = document.createElement('td')
+    calories_entered.innerHTML = calories;
+    new_row.appendChild(calories_entered)
+
+    table.appendChild(new_row);
+}
+
+function clear_table() {
+    document.querySelectorAll('#output > tr:not(:first-child)').forEach(child => child.remove());
+    total = 0;
+}
