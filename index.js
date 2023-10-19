f99746fb95cfb9737490aaebabc98ca19940ae21
dylan33smith <dysm3436@colorado.edu>
2023-10-19 17:36:41 -0600

    changes to js, css, and html

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
-const table = document.getElementById('output');
-let total = 0;
+const test_table = document.getElementById('output');
+let total_cals = 0;
 
 function cal_out() {
     const calories = Number(document.getElementById("calories").value);
-    total += calories;
+    total_cals += calories;
 
     const new_row = document.createElement('tr');
 
     const calories_consumed = document.createElement('td')
-    calories_consumed.innerText = total;
+    calories_consumed.innerText = total_cals;
     new_row.appendChild(calories_consumed);
 
     const calories_entered = document.createElement('td')
     calories_entered.innerHTML = calories;
     new_row.appendChild(calories_entered)
 
-    table.appendChild(new_row);
+    test_table.appendChild(new_row);
 }
 
 function clear_table() {
