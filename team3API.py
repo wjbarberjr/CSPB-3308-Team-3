1754a29538a6f58831af458dbc51698123dfbaa0
wjbarberjr <wiba6472@colorado.edu>
2023-11-09 20:14:35 +0000

    Database functions are working with unit tests

---
+++ team3API.py
@@ -0,0 +1,75 @@
+###############################################################################
+##
+## Author: William Barber
+## Date: November 2023
+## Purpose: To setup a database for user information on Team 3 fitness app
+## Code will be used to supplement other team members database information
+##
+###############################################################################
+import sqlite3
+import os
+
+
+
+def create_database(filename):
+    if not filename.endswith('.db'):
+        filename += '.db'
+
+    if os.path.exists(filename):
+        #print("Database already exists")
+        return
+
+    try:
+        with sqlite3.connect(filename) as conn:
+            cursor = conn.cursor()
+            cursor.execute('''
+                CREATE TABLE IF NOT EXISTS users (
+                    id INTEGER PRIMARY KEY,
+                    first_name TEXT,
+                    last_name TEXT,
+                    dob TEXT,
+                    gender TEXT,
+                    login_name TEXT,
+                    email TEXT
+                )
+            ''')
+        #print("Database created successfully!")
+
+    except sqlite3.Error as e:
+        print(f"Error creating database: {e}")
+        
+
+
+def add_user(first_name, last_name, dob, gender, login_name, email, filename):  #Function to add a user entry
+    conn = sqlite3.connect(filename)                                     #Connect to database
+    cursor = conn.cursor()                                               #Connect to cursor and execute entry
+    cursor.execute('''
+        INSERT INTO users (first_name, last_name, dob, gender, login_name, email)
+        VALUES (?, ?, ?, ?, ?, ?)
+    ''', (first_name, last_name, dob, gender, login_name, email))
+    conn.commit()                                                        #Commit and closeo connection
+    conn.close()
+
+    
+
+def edit_user(user_id, first_name, last_name, dob, gender, login_name, email, filename):  #Function to edit a user entry by id
+    conn = sqlite3.connect(filename)                                     #Connect to database
+    cursor = conn.cursor()                                               #Connect to cursor and execute update
+    cursor.execute('''
+        UPDATE users
+        SET first_name=?, last_name=?, dob=?, gender=?, login_name=?, email=?
+        WHERE id=?
+    ''', (first_name, last_name, dob, gender, login_name, email, user_id))
+    conn.commit()                                                        #Commit and close connection
+    conn.close()
+
+    
+
+def delete_user(user_id, filename):                                      #Function to delete a user entry by id
+    conn = sqlite3.connect(filename)                                     #Connect to database
+    cursor = conn.cursor()                                               #Connect to cursor and execute entry
+    cursor.execute('DELETE FROM users WHERE id=?', (user_id,))           #Delete a specific user id
+    conn.commit()                                                        #Commit and closeo connection
+    conn.close()
+
+    
---
+++ test_team3API.py
@@ -0,0 +1,94 @@
+###############################################################################
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+###############################################################################
+# Purpose: Script to test team3API.py functions to ensure database is set up properly
+# Author: William Barber
+# Usage: This code will be used in Team 3 application development
+# Date: November 2023
+###############################################################################
+import sqlite3
+import team3API
+import unittest
+import os
+
+
+class TestTeam3APIFunctions(unittest.TestCase):
+    
+    def setUp(self):                                                       #Set up a test database or any other necessary setup 
+        self.test_db_filename = 'test_user_database.db'
+        team3API.create_database(self.test_db_filename)
+        pass
+
+    
+    
+    def tearDown(self):                                                    #Clean up after each test
+        if hasattr(self, 'test_db_filename'):
+            conn = sqlite3.connect(self.test_db_filename)
+            conn.close()
+            os.remove(self.test_db_filename)
+        pass
+
+    
+    
+    def test_create_database(self):                                        #Test the create_database function
+        test_db_filename = 'Functiontest.db'
+        team3API.create_database(self.test_db_filename)                    #Create Database
+        self.assertTrue(os.path.exists(self.test_db_filename))             #Assert that the database file was created
+        
+        
+
+    def test_add_user(self):                                               #Test the add_user function
+        self.test_db_filename = 'test_user_database.db'
+        team3API.add_user('John', 'Doe', '1990-01-01', 'Male', 'john_doe', 'john.doe@example.com', self.test_db_filename) #Add user and then check it
+        conn = sqlite3.connect(self.test_db_filename)                      #Check if the user was added successfully
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM users WHERE login_name=?", ('john_doe',))
+        user = cursor.fetchone()
+        conn.close()
+        self.assertIsNotNone(user)                                         #Assert that the user exists in the database
+        expected_user_data = ('John', 'Doe', '1990-01-01', 'Male', 'john_doe', 'john.doe@example.com')
+        self.assertEqual(user[1:], expected_user_data)                     #Check if the user data matches the expected values
+
+        
+        
+    def test_edit_user(self):                                              #Test the edit_user function
+        self.test_db_filename = 'test_user_database.db'
+        team3API.add_user('Jane', 'Doe', '1995-05-05', 'Female', 'jane_doe', 'jane.doe@example.com', self.test_db_filename)
+        conn = sqlite3.connect(self.test_db_filename)                      #Fetch the user ID of the added user from the database
+        cursor = conn.cursor()
+        cursor.execute("SELECT id FROM users WHERE login_name=?", ('jane_doe',))
+        user_id = cursor.fetchone()[0]
+        conn.close()
+                                                                           #Call the edit_user function to edit the user
+        team3API.edit_user(user_id, 'John', 'Doe', '1990-01-01', 'Male', 'john_doe', 'john.doe@example.com', self.test_db_filename)
+        conn = sqlite3.connect(self.test_db_filename)                      #Check if the user was edited successfully
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM users WHERE id=?", (user_id,))
+        edited_user = cursor.fetchone()
+        conn.close()
+        self.assertIsNotNone(edited_user)                                  #Assert that the user exists in the database
+        expected_user_data_after_edit = ('John', 'Doe', '1990-01-01', 'Male', 'john_doe', 'john.doe@example.com')
+        self.assertEqual(edited_user[1:], expected_user_data_after_edit)   #Check if the user data matches the expected values after editing
+        
+        
+    def test_delete_user(self):                                            #Test the delete_user function
+        self.test_db_filename = 'test_user_database.db'
+        team3API.add_user('Mark', 'Smith', '1985-08-15', 'Male', 'mark_smith', 'mark.smith@example.com', self.test_db_filename)
+        conn = sqlite3.connect(self.test_db_filename)                      #Fetch the user ID of the added user from the database
+        cursor = conn.cursor()
+        cursor.execute("SELECT id FROM users WHERE login_name=?", ('mark_smith',))
+        user_id = cursor.fetchone()[0]
+        conn.close()
+        team3API.delete_user(user_id, self.test_db_filename)               #Call the delete_user function to delete the user
+        conn = sqlite3.connect(self.test_db_filename)                      #Check if the user was deleted successfully
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM users WHERE id=?", (user_id,))
+        deleted_user = cursor.fetchone()
+        conn.close()
+        self.assertIsNone(deleted_user)                                    #Assert that the user does not exist in the database
+
+        
+
+if __name__ == '__main__':
+    unittest.main()
