d50abd5d7fa53710b3c8346f6231048d3537cfd8
Jordon Young <64931019+jordon-young@users.noreply.github.com>
2023-12-07 22:56:50 -0500

    Correct spelling

--- database/database.py
+++ database/database.py
@@ -14,7 +14,7 @@ import workouts
 #
 def create_database(db, db_filename):
     # users.create_users(db, db_filename)
-    foods.create_database(db, db_filename)
+    foods.create_foods(db, db_filename)
     exercises.create_exercises(db, db_filename) 
     workouts.create_workouts(db, db_filename) # Requires Users
     exercise_groups.create_exercise_groups(db, db_filename) # Requires Exercises and Workouts
