346093b2732d4545edda4276e5766038f5f75b10
Eric Hernandez <103330888+EWHernandez@users.noreply.github.com>
2023-12-09 15:44:56 -0700

    Update create_exercise_input_db.py

--- create_exercise_input_db.py
+++ create_exercise_input_db.py
@@ -1,44 +1,79 @@
 import sqlite3
 
-# Create the database and establish a connection
-conn = sqlite3.connect('workouts.db')
-cursor = conn.cursor()
+def create_workouts():
+    # Create the database and establish a connection
+    conn = sqlite3.connect('workouts.db')
+    cursor = conn.cursor()
 
-# Create the "workouts" table
-cursor.execute('''
-    CREATE TABLE IF NOT EXISTS users (
-        id INTEGER PRIMARY KEY,
-        exercise_name VARCHAR,
-        date DATETIME,
-        duration TIME,
-        workout_type INT,
-        notes VARCHAR,
-        FOREIGN KEY (user_id) REFERENCES user(id)
-    )
-''')
+    # Create the "workouts" table
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS workouts (
+            id INTEGER PRIMARY KEY,
+            -- FOREIGN KEY (user_id) REFERENCES users (id),
+            date DATETIME,
+            exercise_name VARCHAR,
+            date DATETIME,
+            duration TIME,
+            workout_type INT,
+            notes VARCHAR,
+            FOREIGN KEY (user_id) REFERENCES user(id)
+        )
+    ''')
 
-# Commit changes and close the connection
-conn.commit()
-conn.close()
+    # Commit changes and close the connection
+    conn.commit()
+    conn.close()
 
 # Function to add a workout entry
-def add_workout(exercise_name, date, duration, workout_type, notes):
+#modify to have user Id
+def add_workout(date, exercise_name, duration, workout_type, notes):
     conn = sqlite3.connect('workouts.db')
     cursor = conn.cursor()
     cursor.execute('''
-        INSERT INTO workouts (exercise_name, date, duration, workout_type, notes)
+        INSERT INTO workouts (date, exercise_name, duration, workout_type, notes)
         VALUES (?, ?, ?, ?, ?)
-    ''', (exercise_name, date, duration, workout_type, notes))
+    ''', (date, exercise_name, duration, workout_type, notes))
     conn.commit()
     conn.close()
 
-# Function to delete a user entry by exercise name
+# Function to add a workout entry 'get_workouts(user_ID)'
+def get_workouts():
+    conn = sqlite3.connect('workouts.db')
+    cursor = conn.cursor()
+    cursor.execute('''
+        SELECT date, exercise_name, workout_type, duration,  notes
+        FROM workouts;
+    ''')
+    rows = cursor.fetchall()
+    conn.commit()
+    conn.close()
+    return rows
+
+# Function to delete a user entry by id
 def delete_entry(exercise_name):
     conn = sqlite3.connect('workouts.db')
     cursor = conn.cursor()
-    cursor.execute('DELETE FROM workouts WHERE id=?', (exercise_name))
+    cursor.execute('DELETE FROM workouts WHERE exercise_name=?', (exercise_name))
     conn.commit()
     conn.close()
+
+def recreateTable():
+    conn = sqlite3.connect('workouts.db')
+    cursor = conn.cursor()
+    cursor.execute('DROP TABLE workouts;')
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS workouts (
+        id INTEGER PRIMARY KEY,
+        -- FOREIGN KEY (user_id) REFERENCES users (id),
+        date DATETIME,
+        exercise_name VARCHAR,
+        duration TIME,
+        workout_type INT,
+        notes VARCHAR
+    )''')
+    conn.commit()
+    conn.close()
+
 
 # Example usage:
 # add_workout('Bench Press', '12/1/23', '15', 'strength', '3 sets of 12 reps')
