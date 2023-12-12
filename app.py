37ccd6525217dda477a8b099e5bb045c78600028
William Barber <wiba6472@colorado.edu>
2023-12-11 20:59:41 -0500

    Update

--- app.py
+++ app.py
@@ -246,20 +246,19 @@ def render_exercise_log():
 #   Food 
 #
 
-@app.route('/food_lookup')
+@app.route('/foodlookup')
 def foodlookup():
+    user_id = request.args.get('user_id')
+
     if user_id:
-        user_id = request.args.get('user_id')
-        
-        # If user ID is provided, fetch user details from the database
         user_details = db.users.get_user_by_id(user_id, db_args)
 
         if user_details:
-            # Extract first name from user details
             user_first_name = user_details[1]
             return render_template('foodlookup.html', user_id=user_id, user_first_name=user_first_name)
 
     return render_template('foodlookup.html')
+    
 
 @app.route('/get_food_suggestions') #NO PAGE
 def get_food_suggestions():
